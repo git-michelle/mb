a7fc89c35f974e4e13e4151516a3d9b5b31d2970
Michelle <16948138+michellecan@users.noreply.github.com>
2020-12-30 10:44:42 -0800

    add png project images

--- my-website/src/components/Carousel/ImageData.js
+++ my-website/src/components/Carousel/ImageData.js
@@ -1,23 +1,23 @@
 export const ImageData = [
   {
-    image: "/images/project1.svg",
+    image: "/images/project1.png",
     description: "Master's Thesis",
   },
   {
-    image: "/images/project2.svg",
+    image: "/images/project2.png",
     description: "Cognitive Ergonomics",
   },
   {
-    image: "/images/project3.svg",
+    image: "/images/project3.png",
     description: "Instructional Design and Technology",
   },
   {
-    image: "/images/project4.svg",
+    image: "/images/project4.png",
     description:
       "Design, Implementation, and Evaluation of Humanoid Robots as Cognitive Systems",
   },
   {
-    image: "/images/project5.svg",
+    image: "/images/project5.png",
     description: "List of Courses",
   },
 ];
