3106e22c7a3987865609194b5c7c20af91890ee3
Michelle <16948138+michellecan@users.noreply.github.com>
2021-01-05 11:41:28 -0800

    add netlify html form

--- my-website/src/Components/ContactSection/Contact.js
+++ my-website/src/Components/ContactSection/Contact.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Contact = () => {
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    if (window.location.search.includes("success=true")) {
+      setSuccess(true);
+    }
+  }, []);
   return (
     <section className="contact fullwidth" id="contact">
       <div className="contact-container container-centered-content">
@@ -9,7 +16,13 @@ const Contact = () => {
           <img src="/images/mail-pic.svg" alt="woman writing beside envelope" />
         </div>
         <div className="contact-form-area">
-          <form name="contact" method="POST" data-netlify="true">
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            action="/contact/?success=true"
+          >
+            <input type="hidden" name="form-name" value="contact" />
             <input
               type="text"
               placeholder="Name"
@@ -36,6 +49,9 @@ const Contact = () => {
             <button type="submit" className="btn-outline">
               Send
             </button>
+            {success && (
+              <p style={{ color: "green" }}>Thanks for your message! </p>
+            )}
           </form>
         </div>
       </div>
