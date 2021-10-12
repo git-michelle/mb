39029ff202f87ed34277741c884e61fab140af23
Michelle <16948138+michellecan@users.noreply.github.com>
2021-10-11 21:17:30 -0700

    removed hover-effect, updated skills and about

--- my-website/src/Components/HeroSection/HeroSection.js
+++ my-website/src/Components/HeroSection/HeroSection.js
@@ -1,29 +1,29 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
 const HeroSection = () => {
-  return (
-    <Fragment>
-      <section className="hero">
-        <div className="hero-intro">
-          <h1 className="hero-text-name">Hi, I'm Michelle</h1>
-        </div>
-        <div className="hero-flexbox">
-          <ul className="hero-flexbox-text-area hero-text-ul">
-            <li className="hero-text-li">Front-End Developer</li>
-            <li className="hero-text-li">UX Enthusiast</li>
-          </ul>
-          <div className="hero-flexbox-image-area">
-            <div className="caricature box-shadow"></div>
-          </div>
-        </div>
+	return (
+		<Fragment>
+			<section className="hero">
+				<div className="hero-intro">
+					<h1 className="hero-text-name">Hi, I'm Michelle</h1>
+				</div>
+				<div className="hero-flexbox">
+					<ul className="hero-flexbox-text-area hero-text-ul">
+						<li className="hero-text-li">Software Developer</li>
+						<li className="hero-text-li">UX Enthusiast</li>
+					</ul>
+					<div className="hero-flexbox-image-area">
+						<div className="caricature box-shadow"></div>
+					</div>
+				</div>
 
-        {/* bottom wave  */}
-        <svg className="hero-wave" viewBox="0 0 1960 148">
-          <path d="M0,22.1V148h1960V79.7c-194.4,31.4-348,35.4-445.2,34.2C969,108.6,923.1,1,503.3,0C341.2-0.4,75.2,10.5,0,22.1z" />
-        </svg>
-      </section>
-    </Fragment>
-  );
+				{/* bottom wave  */}
+				<svg className="hero-wave" viewBox="0 0 1960 148">
+					<path d="M0,22.1V148h1960V79.7c-194.4,31.4-348,35.4-445.2,34.2C969,108.6,923.1,1,503.3,0C341.2-0.4,75.2,10.5,0,22.1z" />
+				</svg>
+			</section>
+		</Fragment>
+	);
 };
 
 export default HeroSection;
--- my-website/src/components/SkillsSection/SkillsSection.js
+++ my-website/src/components/SkillsSection/SkillsSection.js
@@ -1,31 +1,31 @@
-import React from "react";
-import SkillsCard from "./SkillsCard";
+import React from 'react';
+import SkillsCard from './SkillsCard';
 
 const SkillSection = () => {
-  return (
-    <section className="skills fullwidth" id="skills">
-      <div className="skills-content container-centered-content">
-        <h2 className="section-heading">Skills</h2>
-        <div className="container-flex">
-          <SkillsCard
-            cardTitle="Front-End"
-            skillsList={["HTML", "CSS3 | SASS", "JavaScript", "React"]}
-          />
-          <SkillsCard
-            cardTitle="Back-End"
-            skillsList={["NodeJS", "Express", "MongoDB", "MySQL"]}
-          />
-          <SkillsCard
-            cardTitle="Design"
-            skillsList={["Figma", "Balsamiq", "Inkscape"]}
-          />
-        </div>
-      </div>
-      <svg className="skills-wave" viewBox="0 0 1960 148">
-        <path d="M0,22.1V148h1960V79.7c-194.4,31.4-348,35.4-445.2,34.2C969,108.6,923.1,1,503.3,0C341.2-0.4,75.2,10.5,0,22.1z" />
-      </svg>
-    </section>
-  );
+	return (
+		<section className="skills fullwidth" id="skills">
+			<div className="skills-content container-centered-content">
+				<h2 className="section-heading">Skills</h2>
+				<div className="container-flex">
+					<SkillsCard
+						cardTitle="Front-End"
+						skillsList={['HTML', 'CSS3 | SASS', 'JavaScript', 'React']}
+					/>
+					<SkillsCard
+						cardTitle="Back-End"
+						skillsList={['NodeJS', 'Express', 'MongoDB', 'MySQL', 'GraphQL']}
+					/>
+					<SkillsCard
+						cardTitle="Design"
+						skillsList={['Figma', 'Balsamiq', 'Inkscape']}
+					/>
+				</div>
+			</div>
+			<svg className="skills-wave" viewBox="0 0 1960 148">
+				<path d="M0,22.1V148h1960V79.7c-194.4,31.4-348,35.4-445.2,34.2C969,108.6,923.1,1,503.3,0C341.2-0.4,75.2,10.5,0,22.1z" />
+			</svg>
+		</section>
+	);
 };
 
 export default SkillSection;
